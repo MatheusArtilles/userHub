f00b5aeb9bea0341fc8d296a0148e4f9b2744e88
MatheusArtilles <mateusaaa2424@gmail.com>
2023-09-04 16:28:29 -0300

    atualizando js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 //base: https://api.github.com/users
-const form = document.querySelector("form");
-form.addEventListener("submit", (event)=> {
-    event.preventDefault();
-
+const urlBase = "https://api.github.com/users";
+const input = document.querySelector("input");
+const btnSubmit = document.querySelector(".btn-submit");
+btnSubmit.addEventListener("click", ()=>{
+    if(input.value == "" || input.value == null){
+        swal({
+            icon: "warning",
+            text: "Insira um usuário válido!",
+            dangerMode: true
+        });
+    } else {
+        fetch(`${urlBase}/${input.value}`)
+            .then(response => {
+                if(response.status == 200 || response.status == 201){
+                    return response.json()
+                }else {
+                    throw response;
+                }
+            }).then(res => {
+                console.log(res);
+            }).catch(err => {
+                if(err.status == 400 || err.status == 401){
+                    alert("erro no servidor, tente mais tarde");
+                }
+            })
+    }
 })
